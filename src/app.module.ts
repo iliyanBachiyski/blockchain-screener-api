59837e5aebded5cb0ff3c4c9a44bb4ef8da0f871
Iliyan Bachiyski <iliyan.bachiyski@limechain.tech>
2025-04-01 11:05:26 +0300

    Enable ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BlockchainController } from './blockchain/blockchain.controller';
-
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController, BlockchainController],
   providers: [AppService],
 })
