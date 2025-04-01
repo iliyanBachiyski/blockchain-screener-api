705a7f1dd651695a9d894b9add6173eaa1b17584
Iliyan Bachiyski <iliyan.bachiyski@limechain.tech>
2025-04-01 16:20:17 +0300

    Add: logger middleware in blockchain module

--- src/blockchain/blockchain.module.ts
+++ src/blockchain/blockchain.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
-import { BlockchainService } from './blockchain.service';
-import { BlockchainController } from './blockchain.controller';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { CoingeckoModule } from 'src/coingecko/coingecko.module';
+import { LoggerMiddleware } from 'src/common/middleware/logger.middleware';
+import { BlockchainController } from './blockchain.controller';
+import { BlockchainService } from './blockchain.service';
 @Module({
   imports: [CoingeckoModule],
   providers: [BlockchainService],
   controllers: [BlockchainController],
 })
-export class BlockchainModule {}
+export class BlockchainModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes(BlockchainController);
+  }
+}
