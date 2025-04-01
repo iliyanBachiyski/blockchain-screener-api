f932661664b1f4e686b0c043fb319ff5868fbac0
Iliyan Bachiyski <iliyan.bachiyski@limechain.tech>
2025-04-01 16:20:46 +0300

    Add: parse int pipe to getExchanges query

--- src/blockchain/blockchain.controller.ts
+++ src/blockchain/blockchain.controller.ts
@@ -1,13 +1,19 @@
-import { Controller, Get, Query } from '@nestjs/common';
-import { BlockchainService } from './blockchain.service';
+import {
+  Controller,
+  DefaultValuePipe,
+  Get,
+  ParseIntPipe,
+  Query,
+} from '@nestjs/common';
 import { Exchange } from 'src/coingecko/types/exchange';
+import { BlockchainService } from './blockchain.service';
 @Controller('blockchain')
 export class BlockchainController {
   constructor(private readonly blockchainService: BlockchainService) {}
   @Get('exchanges')
   getExchanges(
-    @Query('page') page: number = 1,
-    @Query('perPage') perPage: number = 50,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+    @Query('perPage', new DefaultValuePipe(50), ParseIntPipe) perPage: number,
   ): Promise<Exchange[]> {
     return this.blockchainService.getExchanges(page, perPage);
   }
